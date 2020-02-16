d8a71bea017963399d86a4f90850767f9a71f4ba
OKAKI_SE <reeeiapp@gmail.com>
2020-02-17 07:34:01 +0900

    add file

---
+++ mob4.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, c, x int
+
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	fmt.Scan(&x)
+
+	pattern := 0
+
+	for i := 0; i <= a; i++ {
+		for j := 0; j <= b; j++ {
+			for k := 0; k <= c; k++ {
+				if x == (i*500 + j*100 + k*50) {
+					pattern++
+				}
+			}
+		}
+	}
+	fmt.Println(pattern)
+
+}
